99b8bb45b03ea8ed658d51e9441c39c3e8e2d5c5
追梦的人 <27672729+ouyangmeiwen@users.noreply.github.com>
2025-03-22 14:04:56 +0800

    1
    1

--- fastapi/app/dtomap/libItemInventoryInfo_map.py
+++ fastapi/app/dtomap/libItemInventoryInfo_map.py
@@ -12,11 +12,11 @@ class LibItemInventoryInfoMap:
     def model_to_dto(model:LibItemInventoryInfo)->LibItemInventoryInfoDto:
         """ 将 SQLAlchemy 模型转换为 Pydantic DTO """
         json_data = json.dumps({col.name: getattr(model, col.name) for col in model.__table__.columns}, default=str)
-        return LibItemInventoryInfoDto.parse_raw(json_data)
+        return LibItemInventoryInfoDto.model_validate_json(json_data)
 
 
     @staticmethod
     def input_to_model(input:LibItemInventoryInfoInput)->LibItemInventoryInfo:
         """ 将 Pydantic DTO 转换为 SQLAlchemy 模型 """
-        data = json.loads(input.json())
+        data = json.loads(input.model_dump_json())
         return LibItemInventoryInfo(**data)
