1eaf27f69c27514240ac3f795d8bb73768d8ebee
追梦的人 <27672729+ouyangmeiwen@users.noreply.github.com>
2025-03-23 08:31:54 +0800

    1
    1

--- fastapi/app/dtomap/libItemInventoryInfo_map.py
+++ fastapi/app/dtomap/libItemInventoryInfo_map.py
@@ -8,15 +8,28 @@ from app.dtos.libItemInventoryInfo_Input import LibItemInventoryInfoInput
 
 
 class LibItemInventoryInfoMap:
-    @staticmethod
-    def model_to_dto(model:LibItemInventoryInfo)->LibItemInventoryInfoDto:
-        """ 将 SQLAlchemy 模型转换为 Pydantic DTO """
-        json_data = json.dumps({col.name: getattr(model, col.name) for col in model.__table__.columns}, default=str)
-        return LibItemInventoryInfoDto.model_validate_json(json_data)
+    # @staticmethod
+    # def model_to_dto(model:LibItemInventoryInfo)->LibItemInventoryInfoDto:
+    #     """ 将 SQLAlchemy 模型转换为 Pydantic DTO """
+    #     json_data = json.dumps({col.name: getattr(model, col.name) for col in model.__table__.columns}, default=str)
+    #     return LibItemInventoryInfoDto.model_validate_json(json_data)
+
+    # @staticmethod
+    # def input_to_model(input:LibItemInventoryInfoInput)->LibItemInventoryInfo:
+    #     """ 将 Pydantic DTO 转换为 SQLAlchemy 模型 """
+    #     data = json.loads(input.model_dump_json())
+    #     return LibItemInventoryInfo(**data)
 
 
     @staticmethod
-    def input_to_model(input:LibItemInventoryInfoInput)->LibItemInventoryInfo:
-        """ 将 Pydantic DTO 转换为 SQLAlchemy 模型 """
-        data = json.loads(input.model_dump_json())
-        return LibItemInventoryInfo(**data)
+    def model_to_dto(model: LibItemInventoryInfo) -> LibItemInventoryInfoDto:
+        """ 直接解包 SQLAlchemy 模型转换为 Pydantic DTO """
+        data = {k: v for k, v in model.__dict__.items() if not k.startswith('_')}
+        return LibItemInventoryInfoDto(**data)
+
+    @staticmethod
+    def input_to_model(input: LibItemInventoryInfoInput) -> LibItemInventoryInfo:
+        """ 直接解包 Pydantic DTO 转换为 SQLAlchemy 模型 """
+        return LibItemInventoryInfo(**input.model_dump())
+
+
--- fastapi/app/dtomap/libitem_map.py
+++ fastapi/app/dtomap/libitem_map.py
@@ -1,79 +1,62 @@
-
 from app.dbmodel.libitem_model import LibItem
 from app.dtos.libitem_dto import LibitemDto
 from app.utils.stringutils import StringUtils
 from app.dtos.libitem_input import LibitemInput
 
-class LibitenmMap:
+class LibitemMap:
+    # @staticmethod
+    # def model_to_dto(db_model: LibItem) -> LibitemDto:
+    #     dto = LibitemDto()
+    #     for field in db_model.__dict__.keys():
+    #         if hasattr(dto, field):
+    #             value = getattr(db_model, field)
+    #             # 特殊字段转换
+    #             if field in ["IsDeleted", "IsEnable"]:
+    #                 value = StringUtils.to_bool(value)
+    #             setattr(dto, field, value)
+    #     return dto
+
+    # @staticmethod
+    # def input_to_model(input: LibitemInput) -> LibItem:
+    #     dt_model = LibItem()
+    #     for field in input.__dict__.keys():
+    #         if hasattr(dt_model, field):
+    #             value = getattr(input, field)
+    #             # 特殊字段转换
+    #             if field in ["IsDeleted", "IsEnable"]:
+    #                 value = 1 if value else 0
+    #             setattr(dt_model, field, value)
+    #     return dt_model
+    
+    
+    
+    # @staticmethod
+    # def model_to_dto(db_model: LibItem) -> LibitemDto:
+    #     data = db_model.__dict__.copy()
+    #     data["IsDeleted"] = StringUtils.to_bool(data.get("IsDeleted", 0))
+    #     data["IsEnable"] = StringUtils.to_bool(data.get("IsEnable", 0))
+    #     return LibitemDto(**data)
+
+    # @staticmethod
+    # def input_to_model(input: LibitemInput) -> LibItem:
+    #     data = input.__dict__.copy()
+    #     data["IsDeleted"] = 1 if data.get("IsDeleted", False) else 0
+    #     data["IsEnable"] = 1 if data.get("IsEnable", False) else 0
+    #     return LibItem(**data)
+
+
     @staticmethod
-    def model_to_dto(db_model:LibItem)->LibitemDto:
-        dto=LibitemDto(Id=db_model.Id)
-        dto.CreationTime=db_model.CreationTime
-        dto.CreatorUserId=db_model.CreatorUserId
-        dto.LastModificationTime=db_model.LastModificationTime
-        dto.IsDeleted=StringUtils.to_bool(db_model.IsDeleted)   #db是int，dto 是bool
-        dto.DeleterUserId=db_model.DeleterUserId
-        dto.DeletionTime=db_model.DeletionTime
-        dto.InfoId=db_model.InfoId
-        dto.Title=db_model.Title
-        dto.Author=db_model.Author
-        dto.Barcode=db_model.Barcode
-        dto.IsEnable=StringUtils.to_bool(db_model.IsEnable)     #db是int，dto 是bool
-        dto.CallNo=db_model.CallNo
-        dto.PreCallNo=db_model.PreCallNo
-        dto.CatalogCode=db_model.CatalogCode
-        dto.ItemState=db_model.ItemState
-        dto.PressmarkId=db_model.PressmarkId
-        dto.PressmarkName=db_model.PressmarkName
-        dto.LocationId=db_model.LocationId
-        dto.LocationName=db_model.LocationName
-        dto.BookBarcode=db_model.BookBarcode
-        dto.ISBN=db_model.ISBN
-        dto.PubNo=db_model.PubNo
-        dto.Publisher=db_model.Publisher
-        dto.PubDate=db_model.PubDate
-        dto.Price=db_model.Price
-        dto.Pages=db_model.Pages
-        dto.Summary=db_model.Summary
-        dto.ItemType=db_model.ItemType
-        dto.Remark=db_model.Remark
-        dto.OriginType=db_model.OriginType
-        dto.CreateType=db_model.CreateType
-        dto.TenantId=db_model.TenantId
-        return dto
+    def model_to_dto(db_model: LibItem) -> LibitemDto:
+        """ 直接解包 SQLAlchemy 模型为 Pydantic DTO，并转换特殊字段 """
+        data = {k: v for k, v in db_model.__dict__.items() if not k.startswith("_")}  # 过滤 SQLAlchemy 内部字段
+        data["IsDeleted"] = StringUtils.to_bool(data.get("IsDeleted", 0))
+        data["IsEnable"] = StringUtils.to_bool(data.get("IsEnable", 0))
+        return LibitemDto(**data)
+
     @staticmethod
-    def input_to_model(input:LibitemInput)->LibItem:
-        dt_model=LibItem(Id=input.Id)
-        dt_model.CreationTime=input.CreationTime
-        dt_model.CreatorUserId=input.CreatorUserId
-        dt_model.LastModificationTime=input.LastModificationTime
-        dt_model.IsDeleted=1 if input.IsDeleted else 0
-        dt_model.DeleterUserId=input.DeleterUserId
-        dt_model.DeletionTime=input.DeletionTime
-        dt_model.InfoId=input.InfoId
-        dt_model.Title=input.Title
-        dt_model.Author=input.Author
-        dt_model.Barcode=input.Barcode
-        dt_model.IsEnable=1 if input.IsEnable else 0
-        dt_model.CallNo=input.CallNo
-        dt_model.PreCallNo=input.PreCallNo
-        dt_model.CatalogCode=input.CatalogCode
-        dt_model.ItemState=input.ItemState
-        dt_model.PressmarkId=input.PressmarkId
-        dt_model.PressmarkName=input.PressmarkName
-        dt_model.LocationId=input.LocationId
-        dt_model.LocationName=input.LocationName
-        dt_model.BookBarcode=input.BookBarcode
-        dt_model.ISBN=input.ISBN
-        dt_model.PubNo=input.PubNo
-        dt_model.Publisher=input.Publisher
-        dt_model.PubDate=input.PubDate
-        dt_model.Price=input.Price
-        dt_model.Pages=input.Pages
-        dt_model.Summary=input.Summary
-        dt_model.ItemType=input.ItemType
-        dt_model.Remark=input.Remark
-        dt_model.OriginType=input.OriginType
-        dt_model.CreateType=input.CreateType
-        dt_model.TenantId=input.TenantId
-        return dt_model
+    def input_to_model(input: LibitemInput) -> LibItem:
+        """ 直接解包 Pydantic DTO 为 SQLAlchemy 模型，并转换特殊字段 """
+        data = input.model_dump()  # 使用 Pydantic 官方推荐方法
+        data["IsDeleted"] = int(data.get("IsDeleted", False))  # 直接转换成 0/1
+        data["IsEnable"] = int(data.get("IsEnable", False))  
+        return LibItem(**data)
